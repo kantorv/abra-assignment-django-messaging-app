df89b1c11a512f85045ab06b4b0921edfe339a8c
Valentin Kantor <kantorvv@gmail.com>
2023-08-06 15:22:44 +0300

    user admin section fix

--- messagingapp/core/admin.py
+++ messagingapp/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(DefaultUserAdmin):
     pass
