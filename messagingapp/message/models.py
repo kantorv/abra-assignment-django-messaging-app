8a2cc960817adc4adcfdd959931c3cbcb3b6aa45
Valentin Kantor <kantorvv@gmail.com>
2023-08-06 21:06:39 +0300

    messages app implemented

---
+++ messagingapp/message/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from core.models import User
+import uuid
+# Create your models here.
+
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subject = models.CharField(max_length = 255, default="New message")
+    message = models.TextField(default="")
+    created = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    sender = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True, related_name="sent_messages")
+    receiver = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name="received_messages")
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        ordering = ["-created"]
