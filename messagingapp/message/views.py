f18a20a19667fb0134be6f183a42978ec7942167
Valentin Kantor <kantorvv@gmail.com>
2023-08-07 10:10:00 +0300

    create message api method implemented

--- messagingapp/message/views.py
+++ messagingapp/message/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from .models import Message
+from core.models import User
+from .serializers import MessageSerializer
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.http import Http404
+from django.db.models import Q
 
 # Create your views here.
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ModelViewSet automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Message.objects.none()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        user= self.request.user
+
+        # viewing particular message is allowed (just) to sender or receiver
+        q1 = Q(sender=user)
+        q2 = Q(receiver=user)
+
+        try:
+            object =  Message.objects.get(q1|q2, pk=pk)
+        except Message.DoesNotExist:
+            raise Http404
+
+        return object
+
+    def create(self, request, *args, **kwargs):
+        payload = request.data
+        sender = self.request.user
+        receiver_username = payload.get('to')
+        subject = payload.get('subject')
+        message = payload.get('message')
+
+        receiver = User.objects.get(username=receiver_username)
+        data = dict(
+            sender = sender.pk,
+            receiver = receiver.pk,
+            subject = subject,
+            message = message
+        )
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # def perform_create(self, serializer):
+    #     serializer.save(sender=self.request.user)
