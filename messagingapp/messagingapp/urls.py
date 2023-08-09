7b14768dad749b5d9e10f3c8443fe0bdb9e3372c
Valentin Kantor <kantorvv@gmail.com>
2023-08-09 16:48:51 +0300

    token verification endpoint added

--- messagingapp/messagingapp/urls.py
+++ messagingapp/messagingapp/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 
@@ -31,5 +32,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/', include('message.urls'))
 ]
